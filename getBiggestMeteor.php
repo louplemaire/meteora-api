7a3c219c00c7c933ff28bb1d068b70520c3c6d7a
louplemaire <44206840+louplemaire@users.noreply.github.com>
2020-06-16 10:33:35 +0200

    :sparkles: getBiggestMeteor.php > Create an API End Point

---
+++ getBiggestMeteor.php
@@ -0,0 +1,11 @@
+<?php
+    // Config
+    include './includes/config.php';
+
+    // Select meteors
+    $query = $pdo->prepare('SELECT id, name, mass, year FROM meteorite_landings WHERE year = :year ORDER BY mass DESC LIMIT 1');
+    $query->bindValue(':year', $_GET['year'], PDO::PARAM_INT);
+    $query->execute();
+    $meteors = $query->fetchAll();
+
+    echo json_encode($meteors);
