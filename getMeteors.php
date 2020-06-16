8b5c38986f7c8cca998d474909294a696730eca4
louplemaire <44206840+louplemaire@users.noreply.github.com>
2020-06-16 09:25:29 +0200

    :art: getMeteors.php > Improve the select

--- getMeteors.php
+++ getMeteors.php
@@ -3,7 +3,7 @@
     include './includes/config.php';
 
     // Select meteors
-    $query = $pdo->prepare('SELECT id, name, reclat, reclong, year, found FROM meteorite_landings WHERE year = :year');
+    $query = $pdo->prepare('SELECT id, name, found, year, reclat, reclong FROM meteorite_landings WHERE year = :year');
     $query->bindValue(':year', $_GET['year'], PDO::PARAM_INT);
     $query->execute();
     $meteors = $query->fetchAll();
