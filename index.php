2e80ef64f26b425a528bb2317217de975252687e
RomainPct <romain.penchenat@gmail.com>
2020-06-16 15:33:39 +0200

    Improve index

--- index.php
+++ index.php
@@ -1,2 +1,13 @@
-<?php
-    echo 'Hello';
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Meteora API</title>
+</head>
+<body>
+    <h1>Welcome in Meteora API</h1>
+    <p>Meteora API is an API based on a data source from NASA.</p>
+    <p><i>This project was made for educational purposes only - School project at Hetic</i></p>
+</body>
+</html>
