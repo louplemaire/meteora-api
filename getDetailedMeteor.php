abac172baf48f04da734237c0b970c28667ef806
louplemaire <44206840+louplemaire@users.noreply.github.com>
2020-06-18 15:39:36 +0200

    :sparkles: getDetailedMeteor.php > Add city country and flag

--- getDetailedMeteor.php
+++ getDetailedMeteor.php
@@ -6,6 +6,15 @@
     $query = $pdo->prepare('SELECT id, name, recclass, mass, found, year, reclat, reclong FROM meteorite_landings WHERE id = :id');
     $query->bindValue(':id', $_GET['id'], PDO::PARAM_INT);
     $query->execute();
-    $meteors = $query->fetch();
+    $meteor = $query->fetch();
 
-    echo json_encode($meteors);
+    // Fetch OpenCage API
+    $url = 'https://api.opencagedata.com/geocode/v1/json?q='.$meteor->reclat.'+'.$meteor->reclong.'&key='.OPEN_CAGE_API_KEY;
+    $data = @file_get_contents($url);
+    $data = json_decode($data);
+
+    $meteor->city = $data->results[0]->components->city;
+    $meteor->country = $data->results[0]->components->country;
+    $meteor->flag = $data->results[0]->annotations->flag;
+
+    echo json_encode($meteor);
