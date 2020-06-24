79ed63f0bb6b0f985641ec042597813bed437960
louplemaire <44206840+louplemaire@users.noreply.github.com>
2020-06-24 15:01:39 +0200

    :speech_balloon: Index.php > Add text

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 </head>
 <body>
     <h1>Welcome in METEORA API</h1>
-    <p><strong>METEORA API</strong> is based on <strong>NASA's Open Data</strong> and <strong>OpenCage API</strong>.</p>
+    <p><strong>METEORA API</strong> gives you meteors informations. This API is based on <strong>NASA's Open Data</strong> and <strong>OpenCage API</strong>.</p>
     <a href="https://github.com/louplemaire/meteora-api" alt="METEORA API repository">Look the repository</a>
     <p><i><strong>WARNING :</strong> This site has been created for educational purposes as part of the Grande Ecole curriculum of the HETIC school. The contents and data presented have not been the subject of a request for right of use. This site will in no case be exploited for commercial purposes.</i></p>
 </body>
