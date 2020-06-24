e1b91c7784d7acadf527f76812b3358dc16edc72
louplemaire <44206840+louplemaire@users.noreply.github.com>
2020-06-24 14:43:38 +0200

    :speech_balloon: Index.php > Improve text

--- index.php
+++ index.php
@@ -3,11 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Meteora API</title>
+    <title>METEORA API</title>
 </head>
 <body>
-    <h1>Welcome in Meteora API</h1>
-    <p>Meteora API is an API based on a data source from NASA.</p>
-    <p><i>This project was made for educational purposes only - School project at Hetic</i></p>
+    <h1>Welcome in METEORA API</h1>
+    <p><strong>METEORA API</strong> is based on <strong>NASA's Open Data</strong> and <strong>OpenCage API</strong>.</p>
+    <a href="https://github.com/louplemaire/meteora-api" alt="METEORA API repository">Look the repository</a>
+    <p><i><strong>WARNING :</strong> This site has been created for educational purposes as part of the Grande Ecole curriculum of the HETIC school. The contents and data presented have not been the subject of a request for right of use. This site will in no case be exploited for commercial purposes.</i></p>
 </body>
 </html>
